05e0c455115e15446e6cf8216e0bba16a2b2a0e5
jasmehakKaur <69070570+jasmehakKaur@users.noreply.github.com>
2020-11-21 22:19:33 +0530

    Added solution

---
+++ Introductory Problems/Repetitions.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main()
+{
+	string s;
+	cin>>s;
+	int ans=1,c=0;
+	char l='A';
+	for(int i=0;i<s.length();i++)
+	{
+		char d=s[i];
+		if(d==l)
+		{
+			c++;
+			ans=max(c,ans);
+		}
+		else
+		{
+			l=d;
+			c=1;
+		}
+	}
+	cout<<ans;
+ 
+}
